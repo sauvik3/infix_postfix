28b1c2c634726b435754e060a42e6a890b133d70
sauvik3 <sauvik3@gmail.com>
2025-08-05 13:42:58 +0530

    Add infix to postfix stub
    Signed-off-by: sauvik3 <sauvik3@gmail.com>

--- calc/include/calc.hpp
+++ calc/include/calc.hpp
@@ -1,8 +1,13 @@
 // Copyright 2025 Sauvik Roy
 #pragma once
 
+#include <stack>
 #include <string>
+#include <vector>
+
+#include "tokenize.hpp"
 
 namespace calc {
+std::stack<Token> infixToPostfix(const std::vector<Token> &tokens);
 double eval(const std::string &expression);
-}
+}   // namespace calc
--- calc/src/calc.cpp
+++ calc/src/calc.cpp
@@ -1,6 +1,7 @@
 // Copyright 2025 Sauvik Roy
 
 #include <cstdlib>
+#include <stack>
 #include <string>
 #include <vector>
 
@@ -9,15 +10,32 @@
 #include "tokens.hpp"
 
 namespace calc {
+std::stack<Token> infixToPostfix(const std::vector<Token> &tokens) {
+  std::stack<Token> postfix;
+
+  for (const auto &token : tokens) {
+    if (token.second == Operator::OPERAND) {
+      postfix.push(token);
+    }
+  }
+
+  return postfix;
+}
+
 double eval(const std::string &expression) {
   double res = 0.0;
   std::vector<Token> tokens = calc::tokenize(expression);
 
-  for (const auto &token : tokens) {
+  std::stack<Token> postfix = calc::infixToPostfix(tokens);
+
+  while (!postfix.empty()) {
+    auto token = postfix.top();
     if (token.second == Operator::OPERAND) {
       res += std::atof(token.first.c_str());
     }
+    postfix.pop();
   }
+
   return res;
 }
 }   // namespace calc
